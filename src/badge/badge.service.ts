794d041ad4de3f3a2497b2c823f87899e8a5aad0
JH8459 <kk_ong2233@naver.com>
2024-12-20 00:08:47 +0900

    Modify: 프로그래머스 뱃지 디자인 수정

--- src/badge/badge.service.ts
+++ src/badge/badge.service.ts
@@ -3,7 +3,7 @@ import { ProgrammersService } from '../programmers/programmers.service';
 import * as fs from 'fs';
 import * as path from 'path';
 import { UserData } from '../interface/programmers.interface';
-import { getMiniSvgStr, getFullSvgStr } from './util/svg.util';
+import { getSvgStr } from './util/svg.util';
 import { commitFile } from './util/commit.util';
 import { ConfigService } from '@nestjs/config';
 
@@ -31,19 +31,16 @@ export class BadgeService {
     const userData: UserData =
       await this.programmersService.getProgrammersRecordInfo();
 
-    const miniSvgStr: string = getMiniSvgStr(userData);
-    const fullSvgStr: string = getFullSvgStr(userData);
+    const svgStr: string = getSvgStr(userData);
 
     const staticDir = path.join(__dirname, '../../static');
-    const miniSvgPath = path.join(staticDir, 'result_mini.svg');
-    const fullSvgPath = path.join(staticDir, 'result_full.svg');
+    const svgPath = path.join(staticDir, 'result.svg');
 
     if (!fs.existsSync(staticDir)) {
       fs.mkdirSync(staticDir, { recursive: true });
     }
 
-    fs.writeFileSync(miniSvgPath, miniSvgStr);
-    fs.writeFileSync(fullSvgPath, fullSvgStr);
+    fs.writeFileSync(svgPath, svgStr);
 
     this.loggerService.log('✅ Create Programmers badge success');
 
