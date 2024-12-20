37eb215b1de84e75a069b1000826aa0bc6a1a8c2
JH8459 <kk_ong2233@naver.com>
2024-12-20 13:39:45 +0900

    Modify: 뱃지 수정 (리소스 위치 조정)

--- src/badge/util/svg.util.ts
+++ src/badge/util/svg.util.ts
@@ -1,4 +1,6 @@
 import { UserData } from '../../interface/programmers.interface';
+import * as fs from 'fs';
+import * as path from 'path';
 
 /**
  * 뱃지(미니) SVG 문자열 생성 메소드
@@ -7,6 +9,9 @@ import { UserData } from '../../interface/programmers.interface';
  * @returns {string}
  */
 export const getSvgStr = (userData: UserData): string => {
+  const imagePath = path.join(__dirname, '../../../static/skill_check.png');
+  const imageBase64 = fs.readFileSync(imagePath, 'base64');
+
   return `<?xml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="350px" height="170px" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -67,47 +72,57 @@ export const getSvgStr = (userData: UserData): string => {
             <animate attributeName="y2" dur="0.8s" fill="freeze" calcMode="spline" keyTimes="0 ; 0.675 ; 1" keySplines="0 0 1 1 ; 0.5 0 0.5 1" values="50 ; 50 ; 105"/>
         </line>
 
-        <line x1="34" y1="105" x2="67" y2="125" stroke-width="2" stroke="white">
+        <line x1="34" y1="115" x2="67" y2="135" stroke-width="2" stroke="white">
             <animate attributeName="x2" dur="1s" fill="freeze" calcMode="spline" keyTimes="0 ; 0.8 ; 1" keySplines="0 0 1 1 ; 0.5 0 0.5 1" values="34 ; 34 ; 67"/>
+
             <animate attributeName="y2" dur="1s" fill="freeze" calcMode="spline" keyTimes="0 ; 0.8 ; 1" keySplines="0 0 1 1 ; 0.5 0 0.5 1" values="105 ; 105 ; 125"/>
         </line>
 
-        <line x1="67" y1="125" x2="100" y2="105" stroke-width="2" stroke="white">
+        <line x1="67" y1="135" x2="100" y2="115" stroke-width="2" stroke="white">
             <animate attributeName="x2" dur="1.2s" fill="freeze" calcMode="spline" keyTimes="0 ; 0.83333 ; 1" keySplines="0 0 1 1 ; 0.5 0 0.5 1" values="67 ; 67 ; 100"/>
+
             <animate attributeName="y2" dur="1.2s" fill="freeze" calcMode="spline" keyTimes="0 ; 0.83333 ; 1" keySplines="0 0 1 1 ; 0.5 0 0.5 1" values="125 ; 125 ; 105"/>
         </line>
 
-        <line x1="100" y1="105" x2="100" y2="50" stroke-width="2" stroke="white">
+        <line x1="100" y1="115" x2="100" y2="60" stroke-width="2" stroke="white">
             <animate attributeName="y2" dur="1.5s" fill="freeze" calcMode="spline" keyTimes="0 ; 0.8 ; 1" keySplines="0 0 1 1 ; 0.5 0 0.5 1" values="105 ; 105 ; 50"/>
         </line>
 
-        <line x1="67" y1="130" x2="34" y2="110" stroke-width="2" stroke="white">
+        <line x1="67" y1="140" x2="34" y2="120" stroke-width="2" stroke="white">
             <animate attributeName="x2" dur="1.9s" fill="freeze" calcMode="spline" keyTimes="0 ; 0.78947; 1" keySplines="0 0 1 1 ; 0.5 0 0.5 1" values="67 ; 67 ; 34"/>
+
             <animate attributeName="y2" dur="1.9s" fill="freeze" calcMode="spline" keyTimes="0 ; 0.78947 ; 1" keySplines="0 0 1 1 ; 0.5 0 0.5 1" values="130 ; 130 ; 110"/>
         </line>
 
-        <line x1="67" y1="130" x2="100" y2="110" stroke-width="2" stroke="white">
+        <line x1="67" y1="140" x2="100" y2="120" stroke-width="2" stroke="white">
             <animate attributeName="x2" dur="1.9s" fill="freeze" calcMode="spline" keyTimes="0 ; 0.78947 ; 1" keySplines="0 0 1 1 ; 0.5 0 0.5 1" values="67 ; 67 ; 100"/>
+
             <animate attributeName="y2" dur="1.9s" fill="freeze" calcMode="spline" keyTimes="0 ; 0.78947 ; 1" keySplines="0 0 1 1 ; 0.5 0 0.5 1" values="130 ; 130 ; 110"/>
         </line>
 
         <g class="group">
-            <text text-anchor="middle" dominant-baseline="middle" x="65" y="85" class="title_no" style="fill:#ffffff;">${userData.skillCheck.level}</text>
+            <image href="data:image/png;base64,${imageBase64}" x="15" y="15" height="50px" width="100px"/>
+
+            <text text-anchor="middle" dominant-baseline="middle" x="65" y="95" class="title_no" style="fill:#ffffff;">${userData.skillCheck.level}</text>
+
             <text text-anchor="start" x="150" y="60" class="title_id" style="fill:#ffffff;">${userData.name}</text>
         </g>
 
         <g class="group">
             <text text-anchor="start" x="150" y="100" class="sub_title" style="fill:#ffffff;">Score</text>
+
             <text text-anchor="start" x="240" y="100" class="value" style="fill:#ffffff;">${userData.ranking.score.toLocaleString()}</text>
         </g>
 
         <g class="group">
             <text text-anchor="start" x="150" y="120" class="sub_title" style="fill:#ffffff;">Solved</text>
+
             <text text-anchor="start" x="240" y="120" class="value" style="fill:#ffffff;">${userData.codingTest.solved.toLocaleString()}</text>
         </g>
 
         <g class="group">
             <text text-anchor="start" x="150" y="140" class="sub_title" style="fill:#ffffff;">Rank</text>
+
             <text text-anchor="start" x="240" y="140" class="value" style="fill:#ffffff;">${userData.ranking.rank.toLocaleString()}</text>
         </g>
 
